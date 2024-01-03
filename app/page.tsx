9464d995f4ff402a4012e588eee42467605e826e
Michael Gatmaitan <mchlgtmtn@gmail.com>
2024-01-03 21:39:13 +0800

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,11 @@
 export default async function Home() {
   const req = await fetch(process.env.BASE_URL + "/api/blogs");
-  let blogs: Blog[];
-
-  try {
-    blogs = await req.json();
-  } catch (e) {
-    console.log(e);
-  }
+  const blogs: Blog[] = await req.json();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Hello
-      {blogs!.map((blog) => (
+      {blogs.map((blog) => (
         <div key={blog._id}>{blog.blogCaption}</div>
       ))}
     </main>
